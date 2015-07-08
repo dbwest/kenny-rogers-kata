2fe72b70dd894847685221cad8ebf0717ee4dc50
David West <david.b.west@gmail.com>
2015-07-08 12:00:47 -0400

    four_of_a_kind method ready for testing

--- hand.rb
+++ hand.rb
@@ -16,15 +16,20 @@ class Hand
   end
 
   def pair?
-    (greatest_kind == 2) && (freq_dist(@hand).values.count(greatest_kind) == 1)
+    (greatest_kind == 2) && (histogram(@hand).values.count(greatest_kind) == 1)
   end
 
-  def freq_dist arr
+  def four_of_a_kind?
+    histogram_counts = histogram(ranks).map {|k,v| v}
+    histogram_counts.sort == [1,4]
+  end
+
+  def histogram arr
     arr.inject(Hash.new(0)){|hsh,n| hsh[n] += 1 ; hsh }
   end
 
   def greatest_kind
-    freq_dist(ranks).values.max
+    histogram(ranks).values.max
   end
 
 end
